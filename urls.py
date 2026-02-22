49ca20835daa9e7c731a4a37860e23cb48f48a33
Saurabh Yadav <143438180+DevSaurabhY@users.noreply.github.com>
2026-02-22 16:16:22 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,7 +4,14 @@ from . import views
 app_name = 'onlinecourse'
 
 urlpatterns = [
-    path('<int:course_id>/submit/', views.submit, name='submit'),
-    path('<int:course_id>/submission/<int:submission_id>/',
-         views.show_exam_result, name='show_exam_result'),
+    path(
+        'course/<int:course_id>/submit/',
+        views.submit,
+        name='submit'
+    ),
+    path(
+        'course/<int:course_id>/submission/<int:submission_id>/',
+        views.show_exam_result,
+        name='show_exam_result'
+    ),
 ]
