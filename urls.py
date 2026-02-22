20e5a057663b6e891209c03d3e18781ed9ba3cba
Saurabh Yadav <143438180+DevSaurabhY@users.noreply.github.com>
2026-02-22 16:05:47 +0530

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'onlinecourse'
+
+urlpatterns = [
+    path('<int:course_id>/submit/', views.submit, name='submit'),
+    path('<int:course_id>/submission/<int:submission_id>/',
+         views.show_exam_result, name='show_exam_result'),
+]
